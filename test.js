8f36a8d375152fecb4af78844d8c9ba929267d49
Zoli7779 <zoltankocsy@gmail.com>
2026-04-30 08:43:06 +0200

    Automata tesztfajl hozzaadasa

---
+++ test.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+console.log("--- Automata Teszt Futtatása ---");
+if (fs.existsSync('./szavazas.db')) {
+    console.log("✅ Sikeres: Az adatbázis fájl létezik.");
+} else {
+    console.log("❌ Hiba: Az adatbázis nem található!");
+}
